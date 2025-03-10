01f2c9c49959db701ec7ef63745797469572d690
King <kingnoobshow@gmail.com>
2025-03-10 18:02:06 +0530

    A guessing game!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 mod hello_world;
+mod guessing_game;
 
 fn main() {
-    // Main Function To Run All Of My Codes.
-    hello_world::main();
+    // Main Function To Run All Of My Codes. Commented out for now so I can do stuff easier.
+    // hello_world::main();
+    // guessing_game::main();
 }
