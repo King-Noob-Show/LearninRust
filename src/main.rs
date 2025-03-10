2f296096093ed400e00d76053240d1168ba35a48
King <kingnoobshow@gmail.com>
2025-03-10 16:17:21 +0530

    Fresh Start

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-mod custom_types;
-mod hello_world;
-mod primitives;
-mod print;
-mod variable_bindings;
-
 fn main() {
     // Main Function To Run All Of My Codes.
-    hello_world::main();
-    print::main();
-    primitives::main();
-    custom_types::main();
-    variable_bindings::main();
 }
