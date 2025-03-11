3e048e3bf0a03bc8d81d1175c7cf8f5720039bcc
King <kingnoobshow@gmail.com>
2025-03-11 09:51:57 +0530

    Variables and Mutability

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-mod hello_world;
 mod guessing_game;
+mod hello_world;
+mod variables_mutability;
 
 fn main() {
     // Main Function To Run All Of My Codes. Commented out for now so I can do stuff easier.
     // hello_world::main();
     // guessing_game::main();
+    variables_mutability::main();
 }
---
+++ src/variables_mutability.rs
@@ -0,0 +1,30 @@
+pub fn main() {
+    println!("Variables!");
+    variables();
+}
+
+fn variables() {
+    let x = 5; // Unchangeable
+    println!("The value of x is {}", x);
+
+    let mut x = 69; // Shadow + Changeable
+    println!("The value of x is {}", x);
+    x = 69420;
+    println!("The value of x is {}", x);
+    x = 20;
+    println!("The value of x is {}", x);
+
+    const POINTS: i64 = 10000000000;
+    println!("Points: {}", POINTS);
+
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
